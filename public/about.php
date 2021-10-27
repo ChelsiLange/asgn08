d803dadd5faa41c78e1136655286fe3967786072
Chelsi Lange <langechelsi@gmail.com>
2021-10-27 15:03:22 -0400

    begins assignment 08

---
+++ public/about.php
@@ -0,0 +1,16 @@
+<?php require_once('../private/initialize.php'); ?>
+
+<?php $page_title = 'About Us'; ?>
+<?php include(SHARED_PATH . '/public_header.php'); ?>
+
+<div id="main">
+
+  <div id="page" class="narrow">
+    <h2>About Us</h2>
+    <p>Birds!</p>
+  </div>
+  
+</div>
+
+
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
